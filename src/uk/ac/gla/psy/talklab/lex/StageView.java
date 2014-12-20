3c44f828a880fae0e74f77b095085ec6d2e9584d
Mickey <pashov.m@gmail.com>
2014-12-20 23:35:42 +0000

    Min API is now 14! Added a big blue button. Added pseudo immersion

--- src/uk/ac/gla/psy/talklab/lex/StageView.java
+++ src/uk/ac/gla/psy/talklab/lex/StageView.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.os.SystemClock;
-import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -94,6 +93,15 @@ public class StageView extends View {
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		super.onWindowFocusChanged(hasFocus);
+	    if (hasFocus) {
+	        this.setSystemUiVisibility(
+	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+	    
 		mMidX = this.getWidth() / 2;
 		mMidY = this.getHeight() / 2;
 		if (mHighlight == null) {
